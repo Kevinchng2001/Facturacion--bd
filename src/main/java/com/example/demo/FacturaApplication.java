3eb27a52c9322ff4a61a230bc3d0eb8017644e2f
ASUS <ASUS@Kevin>
2021-06-01 17:03:34 -0500

    Cambios pedidos

--- src/main/java/com/example/demo/FacturaApplication.java
+++ src/main/java/com/example/demo/FacturaApplication.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 import java.io.FileNotFoundException;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
--- src/main/java/com/example/demo/controlador/Principal.java
+++ src/main/java/com/example/demo/controlador/Principal.java
@@ -36,56 +36,93 @@ public class Principal {
 			entradaTeclado = entradaEscaner.nextInt();
 			switch (entradaTeclado) {
 			case 1:
-				// Gererar el objeto Persona pidiendo los datos por teclado
-				System.out.println("Ingrese el nombre: ");
-				String nombre = entradaEscaner.next();
-				System.out.println("Ingrese el apellido: ");
-				String apellido = entradaEscaner.next();
-				System.out.println("Ingrese la cédula: ");
-				String cedula = entradaEscaner.next();
-				System.out.println("Ingrese el teléfono: ");
-				String telefono = entradaEscaner.next();
+				try {
+					// Gererar el objeto Persona pidiendo los datos por teclado
+					System.out.println("Ingrese el nombre: ");
+					String nombre = entradaEscaner.next();
+					System.out.println("Ingrese el apellido: ");
+					String apellido = entradaEscaner.next();
+					System.out.println("Ingrese el teléfono: ");
+					String telefono = entradaEscaner.next();
+					System.out.println("Ingrese la cédula: ");
+					String cedula = entradaEscaner.next();
 
-				Persona p = new Persona(nombre, apellido, telefono, cedula);
-				personaRepositorio.save(p);
+					int a, s = 0, ac, r = 0, ult;
+					for (int i = 0; i < cedula.length() - 1; i++) {
+						a = Integer.parseInt(cedula.charAt(i) + "");
+						if (i % 2 == 0) {
+							a = a * 2;
+							if (a > 9) {
+								a = a - 9;
+							}
+						}
+						s = s + a;
+						if (s % 10 != 0) {
+							ac = ((s / 10) + 1) * 10;
+							r = ac - s;
+						}
+						ult = Integer.parseInt(cedula.charAt(9) + "");
+
+						if (ult == r) {
+							System.out.println("La cedula ingresada es correcta");
+						}
+					}
+
+					Persona p = new Persona(nombre, apellido, telefono, cedula);
+					personaRepositorio.save(p);
+				} catch (Exception e) {
+					System.out.println("La celula ingresada es incorrecta");
+				}
 				break;
 			case 2:
-				// 1 Pedir al usuario que persona quiero modificar "Pedir la cédula"
-				System.out.println("Ingrese la cédula que quiera modificar: ");
-				String c1 = entradaEscaner.next();
-				// 2 ENcontrar a la persona por el número de la cédula
-				Persona personamodificada = personaRepositorio.findByCedula(c1);
-				// 3 modificar y cambiar los datos
-				System.out.println("Ingrese la cédula nueva: ");
-				String cac = entradaEscaner.next();
-				personamodificada.setCedula(cac);
-				personaRepositorio.save(personamodificada);
-
+				try {
+					// 1 Pedir al usuario que persona quiero modificar "Pedir la cédula"
+					System.out.println("Ingrese la cédula que quiera modificar: ");
+					String c1 = entradaEscaner.next();
+					// 2 ENcontrar a la persona por el número de la cédula
+					Persona personamodificada = personaRepositorio.findByCedula(c1);
+					// 3 modificar y cambiar los datos
+					System.out.println("Ingrese la cédula nueva: ");
+					String cac = entradaEscaner.next();
+					personamodificada.setCedula(cac);
+					personaRepositorio.save(personamodificada);
+				} catch (Exception e) {
+					System.out.println("La celula ingresada no existe");
+				}
 				break;
 			case 3:
 				try {
-					System.out.println("Ingrese el número de cédula que desea eliminar: ");
-					String cedulaBuscar = entradaEscaner.next();
+					System.out.println("Ingrese el nombre que quiera eliminar: ");
+					String nomb = entradaEscaner.next();
+					List<Persona> personanom = personaRepositorio.findByNombre(nomb);
+					System.out.println("Id|" + "\t" + "|Cedula|" +"\t" + "|Nombre|" + "\t" + "|Apellido|");
+					for (Persona persona : personanom) {
+						System.out.println(persona.getId()+ " " +persona.getCedula() + " " + persona.getNombre() + " " + persona.getApellido());
+					}
+					System.out.println("Ingrese el id que quiera eliminar: ");
+					int id = entradaEscaner.nextInt();
+					Persona personaid = personaRepositorio.getOne(id);
+					personaRepositorio.delete(personaid);
 
-					Persona personaRecuperada = personaRepositorio.findByCedula(cedulaBuscar);
-					personaRepositorio.delete(personaRecuperada);
 					// 1 Pedir al usuario que persona quiero modificar "Pedir la cédula"
 					// 2 ENcontrar a la persona por el número de la cédula
 					// 3 Eliminar
+
 				} catch (Exception e) {
 					System.out.println("Existe un problema con la cédula ingresada ");
 				}
 				break;
 			case 4:
-
-				List<Persona> personas = personaRepositorio.buscarPorNombreLike("Gabriela");
+				// Buscar todas las personas que están en la BD e imprimir os resultados
+				System.out.println("Ingrese el nombre que desee buscar: ");
+				String nom = entradaEscaner.next();
+				List<Persona> personas = personaRepositorio.findByNombre(nom);
 				System.out.println("Cédula|" + "\t" + "|Nombre|" + "\t" + "|Apellido|");
 				for (Persona persona : personas) {
 					System.out.println(persona.getCedula() + " " + persona.getNombre() + " " + persona.getApellido());
 				}
 				break;
 			case 5:
-
 				System.exit(0);
 				break;
 
@@ -93,6 +130,7 @@ public class Principal {
 				break;
 			}
 		} while (entradaTeclado != 5);
+
 
 	}
 
